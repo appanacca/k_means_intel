c84399ab3f5972447d1855dc57d7244b43728655
appanacca <nico.lumi@gmail.com>
2017-07-10 18:17:37 +0200

    structure of arrays version of the code

--- main_kmeans.cpp
+++ main_kmeans.cpp
@@ -4,10 +4,12 @@
 #include <chrono>
 #include <fstream>
 
-	struct Pixel{
-		float r;
-		float g;
-		float b;
+	class PixelVector{
+	public:
+		std::vector<float> r;
+		std::vector<float> g;
+		std::vector<float> b;
+		PixelVector(const std::ptrdiff_t n) : r(n), g(n), b(n) {}
 	};
 
 
@@ -19,11 +21,10 @@ int main(int argc, char const *argv[])
 	const std::size_t nb_iter = 10;
 
 
-	
-	std::vector<Pixel> point(nb_point);
+	PixelVector point(nb_point);
 	std::vector<std::size_t> cluster(nb_point);
 
-	std::vector<Pixel> centroid(nb_cluster);
+	PixelVector centroid(nb_cluster);
 	std::vector<std::size_t> point_per_cluster(nb_cluster);
 
 	std::default_random_engine engine{};
@@ -33,9 +34,9 @@ int main(int argc, char const *argv[])
 
     // inizialization of the points and their cluster
 	for(std::size_t k = 0; k<nb_point; ++k){
-		point[k].r = r_dist(engine);
-		point[k].g = r_dist(engine);
-		point[k].b = r_dist(engine);
+		point.r[k] = r_dist(engine);
+		point.g[k] = r_dist(engine);
+		point.b[k] = r_dist(engine);
 		cluster[k] = i_dist(engine);
 		//myfile << point[k].r << "," << point[k].g << "," << point[k].b << "," << cluster[k] << std::endl;
 	}
@@ -48,9 +49,9 @@ int main(int argc, char const *argv[])
 
 		//compute the centroid of the clusters
 		for (std::ptrdiff_t i = 0; i < nb_cluster; ++i){
-			centroid[i].r += r_dist(engine);
-			centroid[i].g += r_dist(engine);
-			centroid[i].b += r_dist(engine);
+			centroid.r[i] += r_dist(engine);
+			centroid.g[i] += r_dist(engine);
+			centroid.b[i] += r_dist(engine);
 			point_per_cluster[i] = 0;
 		}
 
@@ -59,17 +60,17 @@ int main(int argc, char const *argv[])
 
 		for (std::ptrdiff_t k = 0;  k < nb_cluster; ++k){
 			std::ptrdiff_t i = cluster[k];
-			centroid[i].r += point[k].r;
-			centroid[i].g += point[k].g;
-			centroid[i].b += point[k].b;
+			centroid.r[i] += point.r[k];
+			centroid.g[i] += point.g[k];
+			centroid.b[i] += point.b[k];
 			++point_per_cluster[i];
 		}
 
 		for (std::ptrdiff_t i = 0; i < nb_cluster; ++i){
 			std::ptrdiff_t nb_point_cluster = point_per_cluster[i];
-			centroid[i].r /= nb_point_cluster;
-			centroid[i].g /= nb_point_cluster;
-			centroid[i].b /= nb_point_cluster;
+			centroid.r[i] /= nb_point_cluster;
+			centroid.g[i] /= nb_point_cluster;
+			centroid.b[i] /= nb_point_cluster;
 		}
 
 
@@ -78,9 +79,9 @@ int main(int argc, char const *argv[])
 			float best_distance = std::numeric_limits<float>::max();
 			std::ptrdiff_t best_centroid = -1;
 			for (std::ptrdiff_t i = 0; i < nb_cluster; ++i){
-				float x = point[k].r - centroid[i].r;
-				float y = point[k].g - centroid[i].g;
-				float z = point[k].b - centroid[i].b;
+				float x = point.r[k] - centroid.r[i];
+				float y = point.g[k] - centroid.g[i];
+				float z = point.b[k] - centroid.b[i];
 				float distance = x*x + y*y + z*z;
 				if (distance < best_distance) {
 					best_distance = distance;
@@ -101,7 +102,7 @@ int main(int argc, char const *argv[])
     myfile.open ("initial.dat");
 
     for(std::size_t k = 0; k<nb_point; ++k){
-        myfile << point[k].r << "," << point[k].g << "," << point[k].b << "," << cluster[k] << std::endl;
+        myfile << point.r[k] << "," << point.g[k] << "," << point.b[k] << "," << cluster[k] << std::endl;
     }
 
     myfile.close();
